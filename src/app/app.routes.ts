304eb94d6f1405ef8ab3a6a22eb660e7cb09d5e7
SJIMENEZ <stalin.jimenezr@ug.edu.ec>
2024-08-01 10:39:05 -0500

    Se configuro el app.routers para predeterminar la pagina de login por default de inicio de aplicación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,13 @@ import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes =
 [
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
 ];
