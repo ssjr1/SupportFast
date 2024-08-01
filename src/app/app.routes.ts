e30681f65d6622e14c98fb493976d36479de25e0
SJIMENEZ <stalin.jimenezr@ug.edu.ec>
2024-08-01 12:49:46 -0500

    Se agrego la ruta principal en approutests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
 
 export const routes: Routes =
 [
@@ -11,5 +12,9 @@ export const routes: Routes =
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'principal',
+        component: PrincipalComponent
     }
 ];
