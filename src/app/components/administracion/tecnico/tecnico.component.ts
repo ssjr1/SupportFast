2c4e0aba7eb0b7d86a5a0449a9805ddd2f51504c
DevEfren <gilsonecruz92@gmail.com>
2024-08-08 19:05:17 -0500

    Se creo servicio y componente Tecnico con su respectiva tabla para consulta

---
+++ src/app/components/administracion/tecnico/tecnico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico',
+  standalone: true,
+  imports: [],
+  templateUrl: './tecnico.component.html',
+  styleUrl: './tecnico.component.scss'
+})
+export class TecnicoComponent {
+
+}
